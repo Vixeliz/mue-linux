9325b71479b50e1ada0fcaf483d4e259234fbb84
Maxzymus Dominick <muevoid@protonmail.com>
2020-10-13 01:55:47 +0000

    fixes

--- core/libressl/files/update-certdata.sh
+++ core/libressl/files/update-certdata.sh
@@ -1,14 +1,12 @@
 #!/bin/sh -e
-#
-# update-certdata.sh
 
-[ -w "$CPT_ROOT/etc/ssl" ] || {
-    printf '%s\n' "${0##*/}: root required to update CA certificates." >&2
+[ -w /etc/ssl ] || {
+    printf '%s\n' "${0##*/}: root required to update cert." >&2
     exit 1
 }
 
-cd "$CPT_ROOT/etc/ssl" && {
-    wget https://curl.haxx.se/ca/cacert.pem
+cd /etc/ssl && {
+    curl -LO https://curl.haxx.se/ca/cacert.pem
     mv -f cacert.pem cert.pem
     printf '%s\n' "${0##*/}: updated cert.pem"
 }
